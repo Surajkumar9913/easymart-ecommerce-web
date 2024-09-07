e731f99cc5d3b461fd012b48231e0a61d66a40dd
Surajkumar9913 <surajyuva200@gmail.com>
2024-09-07 16:50:25 +0530

    Some changes in stylesheet

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ let cartitems = JSON.parse(localStorage.getItem("cart")) || [];
 (async () => {
     try {
         const response = await fetch('https://fakestoreapi.com/products');
-        const result  = await response.json()
+        const result = await response.json()
 
         const div = document.getElementById("main")
-        for(let i = 0; i < result.length - 12; i++){ 
-            currentIndex = i
+        for (let i = 0; i < result.length - 10; i++) {
+            currentIndex = i+1
             const itemCard = document.createElement("div");
 
             const imgDiv = document.createElement("img");
@@ -19,13 +19,13 @@ let cartitems = JSON.parse(localStorage.getItem("cart")) || [];
 
             const titleDiv = document.createElement("div");
             const divNode = document.createTextNode(` ${result[i].title}`);
-            titleDiv.appendChild(divNode); 
+            titleDiv.appendChild(divNode);
             itemCard.appendChild(titleDiv);
             titleDiv.className = 'title';
 
             const priceDiv = document.createElement("div");
             const pricedivNode = document.createTextNode(` $${result[i].price}`);
-            priceDiv.appendChild(pricedivNode); 
+            priceDiv.appendChild(pricedivNode);
             itemCard.appendChild(priceDiv);
             priceDiv.className = 'price';
 
@@ -38,21 +38,21 @@ let cartitems = JSON.parse(localStorage.getItem("cart")) || [];
             // itemCard.appendChild(offerdiv);
             // offerdiv.className = 'offer';
 
-          
+
             const card_btn_div = document.createElement('div');
             itemCard.appendChild(card_btn_div);
             card_btn_div.className = 'card-btns';
 
-           
+
             const itemdetails = document.createElement('a');
             const itemdetailsNode = document.createTextNode('Order Now');
             itemdetails.href = `singlcard.html?id=${result[i].id}`;
             itemdetails.appendChild(itemdetailsNode);
             card_btn_div.appendChild(itemdetails);
 
-            
+
             const cardbtnDiv = document.createElement('button');
-            cardbtnDiv.onclick = function() {
+            cardbtnDiv.onclick = function () {
                 addTocart(result[i]);
             };
             const cardbtntextNode = document.createTextNode('Add to cart');
@@ -77,78 +77,79 @@ function addTocart(data) {
 
 // display more items after click on button 'view more' 
 
-async function displayMoreitems(){
-  console.log(currentIndex);
+async function displayMoreitems() {
+    console.log(currentIndex);
     try {
 
-       
-       let jcurrentIndex;
+
+        let jcurrentIndex;
         const response = await fetch('https://fakestoreapi.com/products');
-        const result  = await response.json()
-        
+        const result = await response.json()
+
         const div = document.getElementById("main")
-        
-        for(let j = currentIndex ;j<result.length;j++){
 
-            jcurrentIndex = j
+        for (let j = currentIndex; j < result.length; j++) {
 
-        const itemcard = document.createElement('div')
-        //  itemcard.href = `singlcard.html?id=${result[j].id}`
-        const imgDiv = document.createElement('img')
-        imgDiv.src=result[j].image
-        itemcard.appendChild(imgDiv);
+            jcurrentIndex = j+1
 
-        const titleDiv = document.createElement('div')
-        const divNode =document.createTextNode(result[j].title)
-        titleDiv.appendChild(divNode)
-        itemcard.appendChild(titleDiv)
-        titleDiv.className='title'
+            const itemcard = document.createElement('div')
+            //  itemcard.href = `singlcard.html?id=${result[j].id}`
+            const imgDiv = document.createElement('img')
+            imgDiv.src = result[j].image
+            itemcard.appendChild(imgDiv);
 
-        const priceDiv = document.createElement('div')
-        const pricedivNode = document.createTextNode(`$${result[j].price}`)
-        priceDiv.appendChild(pricedivNode)
-        itemcard.appendChild(priceDiv)
-        priceDiv.className='price'
+            const titleDiv = document.createElement('div')
+            const divNode = document.createTextNode(result[j].title)
+            titleDiv.appendChild(divNode)
+            itemcard.appendChild(titleDiv)
+            titleDiv.className = 'title'
 
-
-
-        div.appendChild(itemcard)
-        itemcard.className='card'
-
-        const offerdiv = document.createElement('div')
-           const offerdivNode = document.createTextNode('30%off')
-           offerdiv.appendChild(offerdivNode)
-           itemcard.appendChild(offerdiv)
-           offerdiv.className='offer'
+            const priceDiv = document.createElement('div')
+            const pricedivNode = document.createTextNode(`$${result[j].price}`)
+            priceDiv.appendChild(pricedivNode)
+            itemcard.appendChild(priceDiv)
+            priceDiv.className = 'price'
 
 
-           const card_btn_div = document.createElement('div')
-           itemcard.appendChild(card_btn_div)
-           card_btn_div.className = 'card-btns'
 
-           const itemdetails = document.createElement('a')
-           const itemdetailsNode = document.createTextNode('Order Now')
-           itemdetails.href = `singlcard.html?id=${result[j].id}`
-           itemdetails.appendChild(itemdetailsNode)
-           card_btn_div.appendChild(itemdetails)
-        
-           const cardbtnDiv = document.createElement('button');
-            cardbtnDiv.onclick = function() {
+            div.appendChild(itemcard)
+            itemcard.className = 'card'
+
+            // const offerdiv = document.createElement('div')
+            //    const offerdivNode = document.createTextNode('30%off')
+            //    offerdiv.appendChild(offerdivNode)
+            //    itemcard.appendChild(offerdiv)
+            //    offerdiv.className='offer'
+
+
+            const card_btn_div = document.createElement('div')
+            itemcard.appendChild(card_btn_div)
+            card_btn_div.className = 'card-btns'
+
+            const itemdetails = document.createElement('a')
+            const itemdetailsNode = document.createTextNode('Order Now')
+            itemdetails.href = `singlcard.html?id=${result[j].id}`
+            itemdetails.appendChild(itemdetailsNode)
+            card_btn_div.appendChild(itemdetails)
+
+            const cardbtnDiv = document.createElement('button');
+            cardbtnDiv.onclick = function () {
                 addTocart(result[j]);
             };
-           const cardbtntextNode = document.createTextNode('Add to cart')
-           cardbtnDiv.appendChild(cardbtntextNode)
-           card_btn_div.appendChild(cardbtnDiv)
+            const cardbtntextNode = document.createTextNode('Add to cart')
+            cardbtnDiv.appendChild(cardbtntextNode)
+            card_btn_div.appendChild(cardbtnDiv)
+
+
+
 
         }
-
-
-
-        // if(j == result.length){
-        //     document.querySelector('.viewmore').remove();
-        // }
+        console.log(jcurrentIndex);
+        if (jcurrentIndex == result.length) {
+            document.querySelector('.viewmore').remove();
+        }
     }
-     catch (error) {
+    catch (error) {
         console.log(error);
     }
 
@@ -158,54 +159,53 @@ async function displayMoreitems(){
 
 // search bar functionality
 
-async function search()
-{
+async function search() {
 
     let input = document.getElementById('input')
     console.log(input.value);
     const response = await fetch('https://fakestoreapi.com/products');
-    let result  = await response.json()
-    const n = result.filter((item )=>item.title.includes(input.value))
+    let result = await response.json()
+    const n = result.filter((item) => item.title.includes(input.value))
     console.log(n);
-    
+
 
     const div = document.getElementById("main")
-     div.innerHTML= null
-    for(let i=0;i<n.length;i++){
-     
+    div.innerHTML = null
+    for (let i = 0; i < n.length; i++) {
+
         const itemCard = document.createElement("div")
         const imgDiv = document.createElement("img")
-        imgDiv.src=n[i].image
+        imgDiv.src = n[i].image
         itemCard.appendChild(imgDiv)
-    
-    
-       
-       const titleDiv = document.createElement("div")
-       const divNode = document.createTextNode(`Title: ${n[i].title}`)
-       titleDiv.appendChild(divNode) 
-       itemCard.appendChild(titleDiv)
-       titleDiv.className='title';
-    
-       
-    
-       
-    
-       const priceDiv = document.createElement("div")
-       const pricedivNode = document.createTextNode(`Price: $${n[i].price}`)
-       priceDiv.appendChild(pricedivNode) 
-       itemCard.appendChild(priceDiv)
-       priceDiv.className='price'
-    
-    
-    
-       div.appendChild(itemCard)
-       itemCard.className='card'
-        
-    
+
+
+
+        const titleDiv = document.createElement("div")
+        const divNode = document.createTextNode(`Title: ${n[i].title}`)
+        titleDiv.appendChild(divNode)
+        itemCard.appendChild(titleDiv)
+        titleDiv.className = 'title';
+
+
+
+
+
+        const priceDiv = document.createElement("div")
+        const pricedivNode = document.createTextNode(`Price: $${n[i].price}`)
+        priceDiv.appendChild(pricedivNode)
+        itemCard.appendChild(priceDiv)
+        priceDiv.className = 'price'
+
+
+
+        div.appendChild(itemCard)
+        itemCard.className = 'card'
+
+
 
     }
-    
-  
+
+
 }
 
 
@@ -214,27 +214,27 @@ const params = new URLSearchParams(window.location.search);
 const productId = params.get('id');
 console.log(productId);
 
-if(productId){
+if (productId) {
     (async () => {
         try {
             const response = await fetch(`https://fakestoreapi.com/products/${productId}`);
-            const result  = await response.json()
-    
+            const result = await response.json()
+
             console.log(result);
 
             const div = document.getElementById("productimg")
             const imgDiv = document.createElement("img")
-            imgDiv.src=result.image
+            imgDiv.src = result.image
             div.appendChild(imgDiv)
-    
-    
-                
+
+
+
             const titlediv = document.getElementById("p-title")
             const titlehheading = document.createElement("h1")
             const headingNode = document.createTextNode(` ${result.title}`)
             titlehheading.appendChild(headingNode)
             titlediv.appendChild(titlehheading)
-            
+
 
 
             const descriptiondiv = document.getElementById("description")
@@ -249,12 +249,12 @@ if(productId){
             productpricepara.appendChild(productpriceNode)
             pricediv.appendChild(productpricepara)
 
-           
-          
 
- 
-    
-            
+
+
+
+
+
         } catch (error) {
             console.log(error);
         }
@@ -267,92 +267,90 @@ if(productId){
 const str = 'Save more with coupons & up to 70% Off !'
 
 let count = 1;
-const typingText = ()=>{
+const typingText = () => {
 
     const typingPara = document.getElementById('typingtxt')
-    const returnstr = str.slice(0,count)
+    const returnstr = str.slice(0, count)
 
-    
+
     typingPara.innerHTML = returnstr;
     count++
-    
-    setTimeout(()=>typingText(),100)
+
+    setTimeout(() => typingText(), 100)
 }
 typingText()
-         let prev=0;
-         let next=4;
+let prev = 0;
+let next = 4;
 
-async function itemlistCard(prev, next){
+async function itemlistCard(prev, next) {
     try {
 
         const response = await fetch('https://fakestoreapi.com/products');
-        const data  = await response.json()
-        const result = data.slice(prev,next)
-    
+        const data = await response.json()
+        const result = data.slice(prev, next)
 
-         const div= document.getElementById('productitemlistcard')
-         div.innerHTML = null
-         for(let i=0;i<result.length;i++){
 
-           
+        const div = document.getElementById('productitemlistcard')
+        div.innerHTML = null
+        for (let i = 0; i < result.length; i++) {
+
+
             const itemCard = document.createElement("a")
             itemCard.href = `singlcard.html?id=${result[i].id}`
             const imgDiv = document.createElement("img")
-            imgDiv.src=result[i].image
+            imgDiv.src = result[i].image
             itemCard.appendChild(imgDiv)
 
 
-           
-        //    const titleDiv = document.createElement("div")
-        //    const divNode = document.createTextNode(` ${result[i].title}`)
-        //    titleDiv.appendChild(divNode) 
-        //    itemCard.appendChild(titleDiv)
-        //    titleDiv.className='title';
 
-           
+            //    const titleDiv = document.createElement("div")
+            //    const divNode = document.createTextNode(` ${result[i].title}`)
+            //    titleDiv.appendChild(divNode) 
+            //    itemCard.appendChild(titleDiv)
+            //    titleDiv.className='title';
 
-           const priceDiv = document.createElement("div")
-           const pricedivNode = document.createTextNode(` $${result[i].price}`)
-           priceDiv.appendChild(pricedivNode) 
-           itemCard.appendChild(priceDiv)
-           priceDiv.className='price'
 
-      
 
-           div.appendChild(itemCard)
-           itemCard.className='card'
-         }
-        
-         
+            const priceDiv = document.createElement("div")
+            const pricedivNode = document.createTextNode(` $${result[i].price}`)
+            priceDiv.appendChild(pricedivNode)
+            itemCard.appendChild(priceDiv)
+            priceDiv.className = 'price'
+
+
+
+            div.appendChild(itemCard)
+            itemCard.className = 'card'
+        }
+
+
     } catch (error) {
         console.log(error);
     }
-    
+
 }
 itemlistCard(prev, next);
- 
 
-function decrement(){
-    if(prev==0){
+
+function decrement() {
+    if (prev == 0) {
         return
     }
-    else{
+    else {
         prev--
         next--
         itemlistCard(prev, next);
     }
-    
- }
- function increment(){
-  
+
+}
+function increment() {
+
+
     prev++
     next++
     itemlistCard(prev, next);
-    
-    
+
 }
-
-
 
 
 
